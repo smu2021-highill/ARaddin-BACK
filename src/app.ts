12b6d693814bf700f92a93c9a5a61acb2cf25ed2
dowon <devdw98@gmail.com>
2021-03-17 22:21:02 +0900

    firebase initial

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { initialFirebase } from './utils/firebase';
 
 // Create Express server
 const app = express();
@@ -8,5 +9,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
+
+// firebase connection & use firestore
+export const firebase = initialFirebase().firestore();
 
 export default app;
